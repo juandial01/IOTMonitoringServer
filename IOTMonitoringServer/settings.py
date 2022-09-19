b94d566032b5711714a2df157c54d0583a0bee42
Juan Diego Alzate <jd.alzate253@uniandes.edu.co>
2022-09-19 18:08:09 -0500

    fix ip viewer

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.81.186.24"]
+ALLOWED_HOSTS = ["localhost", "54.84.87.218"]
 
 
 # Application definition
